77d6040b45252c79805802540c50dba4182b83f7
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2022-05-31 09:59:57 -0300

    Add migrations

---
+++ DemoEFCoreWebApi/Migrations/20220531125711_Addfull.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoEFCoreWebApi.Migrations
+{
+    public partial class Addfull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
